d22878307043e8d65db3fb0536fb380194581836
Octavio Luperón <armando2445@outlook.com>
2024-05-16 22:29:32 -0400

    this is me testing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterContentChecked, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   standalone: true,
   imports: [CommonModule],
   templateUrl: './home.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {
+export class HomeComponent implements AfterContentChecked  {
   
+  @ViewChild('P5body')
+  public fatherDiv?: ElementRef<HTMLDivElement>;
+
+  public childDivHeight: number = 0;
+
+  ngAfterContentChecked(): void {
+    console.log(this.fatherDiv?.nativeElement.offsetHeight)
+
+    if(this.fatherDiv)
+
+    // this.childDivHeight = this.fatherDiv.nativeElement.offsetHeight
+    
+    // this.fatherDiv.nativeElement.style.borderBottomWidth = this.childDivHeight + 'px'
+    
+    console.log(this.fatherDiv.nativeElement.offsetHeight)
+  }
+
+
+  
+
 }

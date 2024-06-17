f40c5c8323c75926f53593f26ac4d4b9a45d8834
Octavio Luperón <armando2445@outlook.com>
2024-06-17 15:40:36 -0400

    Made a cool looking login logo

--- src/app/auth/layout/auth-layout/auth-layout.component.ts
+++ src/app/auth/layout/auth-layout/auth-layout.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewChecked, ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,4 +8,28 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './auth-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AuthLayoutComponent { }
+export class AuthLayoutComponent implements AfterViewChecked {
+
+  @ViewChild('title') titleElenmt?: ElementRef<HTMLHeadingElement>;
+  @ViewChild('thediv') theDiv?: ElementRef<HTMLDivElement>
+
+  ngAfterViewChecked(): void {
+
+    // if(!this.titleElenmt?.nativeElement || !this.theDiv?.nativeElement) return
+
+    // const divRect = this.theDiv?.nativeElement.getBoundingClientRect();
+    // const h1Rect = this.titleElenmt?.nativeElement.getBoundingClientRect();
+
+    // // Check if the div overlaps the h1
+    // const overlap = !(divRect.right < h1Rect.left ||
+    //                   divRect.left > h1Rect.right ||
+    //                   divRect.bottom < h1Rect.top ||
+    //                   divRect.top > h1Rect.bottom);
+
+    // // If they overlap, change the color of the h1
+    // if (overlap) {
+    //   this.titleElenmt.nativeElement.style.color = 'white'; // Replace 'newColor' with your desired color
+    // }
+
+  }
+}

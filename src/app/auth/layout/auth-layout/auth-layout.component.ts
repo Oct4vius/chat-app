53bf98c8216ef8d7a1d9a7685adfe53c356df821
Octavio Luperón <armando2445@outlook.com>
2024-06-18 19:08:39 -0400

    fix: now the login is in the router

--- src/app/auth/layout/auth-layout/auth-layout.component.ts
+++ src/app/auth/layout/auth-layout/auth-layout.component.ts
@@ -1,37 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, computed, inject, OnInit } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './auth-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AuthLayoutComponent implements OnInit {
+export class AuthLayoutComponent {
 
 
-  ngOnInit(): void {
-
-    console.log(Object.keys(this.loginForm.controls))
-
-  }
-  private fb = inject(FormBuilder);
-  public loginForm = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required],
-  })
-  public loginElements = computed(() => Object.keys(this.loginForm.controls))
-
-
-  public onSubmit() {
-
-    console.log('errores', this.loginForm.errors)
-
-    if(this.loginForm.invalid) return
-
-    console.log(this.loginForm.value)
-  }
 
 }
--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,11 +1,34 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
+
+  private router = inject(Router)
+  private fb = inject(FormBuilder);
+  public loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  })
+  public loginElements = computed(() => Object.keys(this.loginForm.controls))
+
+
+  public onSubmit() {
+
+    if(this.loginForm.invalid) return
+
+    console.log(this.loginForm.value)
+  }
+
+  public goToRegister() {
+    this.router.navigate(['/auth/register'])
+  }
 
 }
